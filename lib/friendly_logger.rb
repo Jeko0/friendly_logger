314c10108db3f2959711949133c53d009bf467e4
jeko <jekoaphu@gmail.com>
2024-11-04 20:07:57 +0400

    initial commit

---
+++ lib/friendly_logger.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "friendly_logger/version"
+
+module FriendlyLogger
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/friendly_logger_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FriendlyLogger do
+  it "has a version number" do
+    expect(FriendlyLogger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
